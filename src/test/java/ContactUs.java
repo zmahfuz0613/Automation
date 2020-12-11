c0ec58fb43ef907645d1755c5248f294b9007b18
mztech <mztech29@gmail.com>
2020-12-11 11:32:44 -0500

    SearchMovie file added

--- src/test/java/ContactUs.java
+++ src/test/java/ContactUs.java
@@ -1,49 +1,49 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-public class ContactUs extends PageObject {
-
-
-    private final String FULL_NAME = "Zunayed Mahfuz";
-    private final String EMAIL = "zunayed.lm@gmail.com";
-    private final String MESSAGE = "This is selenium message and I hope this works blah blah blah";
-
-    @FindBy(xpath = "//div[@class=\"navbar-nav\"]/a[1]")
-    private WebElement contact_us;
-
-    @FindBy(id = "validationCustom01")
-    private WebElement full_name;
-
-    @FindBy(id = "validationCustom02")
-    private WebElement valid_email;
-
-    @FindBy(id = "validationCustom03")
-    private WebElement text_message;
-
-    @FindBy(xpath = "//div[@class=\"form-group\"]/button")
-    private WebElement submit_button;
-
-    public ContactUs(WebDriver driver) {
-        super(driver);
-    }
-
-    public void clickContactButton() {
-        this.contact_us.click();
-    }
-
-    public void enterFullName() {
-        this.full_name.sendKeys(FULL_NAME);
-    }
-
-    public void enterEmail() {
-        this.valid_email.sendKeys(EMAIL);
-    }
-    public void enterMessage() {
-        this.text_message.sendKeys(MESSAGE);
-    }
-
-    public void pressSubmitButton() {
-        this.submit_button.click();
-    }
-}
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.support.FindBy;
+//
+//public class ContactUs extends PageObject {
+//
+//
+//    private final String FULL_NAME = "Zunayed Mahfuz";
+//    private final String EMAIL = "zunayed.lm@gmail.com";
+//    private final String MESSAGE = "This is selenium message and I hope this works blah blah blah";
+//
+//    @FindBy(xpath = "//div[@class=\"navbar-nav\"]/a[1]")
+//    private WebElement contact_us;
+//
+//    @FindBy(id = "validationCustom01")
+//    private WebElement full_name;
+//
+//    @FindBy(id = "validationCustom02")
+//    private WebElement valid_email;
+//
+//    @FindBy(id = "validationCustom03")
+//    private WebElement text_message;
+//
+//    @FindBy(xpath = "//div[@class=\"form-group\"]/button")
+//    private WebElement submit_button;
+//
+//    public ContactUs(WebDriver driver) {
+//        super(driver);
+//    }
+//
+//    public void clickContactButton() {
+//        this.contact_us.click();
+//    }
+//
+//    public void enterFullName() {
+//        this.full_name.sendKeys(FULL_NAME);
+//    }
+//
+//    public void enterEmail() {
+//        this.valid_email.sendKeys(EMAIL);
+//    }
+//    public void enterMessage() {
+//        this.text_message.sendKeys(MESSAGE);
+//    }
+//
+//    public void pressSubmitButton() {
+//        this.submit_button.click();
+//    }
+//}
--- src/test/java/TestPlan.java
+++ src/test/java/TestPlan.java
@@ -1,55 +1,55 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
-
-public class TestPlan {
-    private static final WebDriver driver = new ChromeDriver();
-
-    @BeforeSuite
-    public static void main(String[] args) {
-        // ChromeDriver location set up in Utils class
-        System.setProperty("webdriver.chrome.driver", Utils.CHROME_DRIVER_LOCATION);
-    }
-
-    @Test(testName = "Search for a Movie Name", priority = 1)
-    public static void searchMovie() throws InterruptedException {
-        driver.get(Utils.BASE_URL);
-        MovieDetails movieDetails = new MovieDetails(driver);
-
-        movieDetails.enterMovieName();
-//      driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
-        Thread.sleep(1000);
-        movieDetails.clickSearchButton();
-    }
-    @Test(testName = "Contact us", priority = 2)
-    public static void contactUs() throws InterruptedException {
-        driver.get(Utils.BASE_URL);
-        ContactUs contactUs = new ContactUs(driver);
-
-        contactUs.clickContactButton();
-
-        contactUs.enterFullName();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        contactUs.enterEmail();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        contactUs.enterMessage();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        contactUs.pressSubmitButton();
-    }
-
-    }
-
-
-
-
-//    @AfterSuite
-//    public static void cleanUp() {
-//        driver.manage().deleteAllCookies();
-//        driver.close();
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.annotations.BeforeSuite;
+//import org.testng.annotations.Test;
+//
+//import java.util.concurrent.TimeUnit;
+//
+//public class TestPlan {
+//    private static final WebDriver driver = new ChromeDriver();
+//
+//    @BeforeSuite
+//    public static void main(String[] args) {
+//        // ChromeDriver location set up in Utils class
+//        System.setProperty("webdriver.chrome.driver", Utils.CHROME_DRIVER_LOCATION);
 //    }
+//
+//    @Test(testName = "Search for a Movie Name", priority = 1)
+//    public static void searchMovie() throws InterruptedException {
+//        driver.get(Utils.BASE_URL);
+//        MovieDetails movieDetails = new MovieDetails(driver);
+//
+//        movieDetails.enterMovieName();
+////      driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+//        Thread.sleep(1000);
+//        movieDetails.clickSearchButton();
+//    }
+//    @Test(testName = "Contact us", priority = 2)
+//    public static void contactUs() throws InterruptedException {
+//        driver.get(Utils.BASE_URL);
+//        ContactUs contactUs = new ContactUs(driver);
+//
+//        contactUs.clickContactButton();
+//
+//        contactUs.enterFullName();
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//
+//        contactUs.enterEmail();
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//
+//        contactUs.enterMessage();
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//
+//        contactUs.pressSubmitButton();
+//    }
+//
+//    }
+//
+//
+//
+//
+////    @AfterSuite
+////    public static void cleanUp() {
+////        driver.manage().deleteAllCookies();
+////        driver.close();
+////    }
